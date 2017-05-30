b8b5d9bd2e4ea6989b00e6fa738f3a083bc9ad23
TheCompSciNoob <kyroschow@gmail.com>
2017-05-30 08:15:05 -0700

    Add files via upload

--- EditScreenManager.java
+++ EditScreenManager.java
@@ -3,8 +3,9 @@ import javax.swing.*;
 import java.awt.Graphics;
 import java.awt.Color;
 import java.util.ArrayList;
-public class EditScreenManager extends JPanel
+public class EditScreenManager extends JPanel implements ActionListener
 {
+  private static final String fwdEditAction1 = "fwdEditAction1", revEditAction1 = "revEditAction1", fwdEditAction2 = "fwdEditAction2", revEditAction2 = "revEditAction2";
   private ArrayList<GamePiece> entities;
   private int playerNum;
   
@@ -12,7 +13,26 @@ public class EditScreenManager extends JPanel
   {
     entities = pieces;
     playerNum = player;
+    createButtons();
+  }
+  
+  @Override
+  protected void paintComponent(Graphics g)
+  {
+    super.paintComponent(g);
   }
   
   //TODO: changes contents of arraylist (in corresponding player) by clicking button
+  
+  private void createButtons()
+  {
+    
+  }
+  
+  @Override
+  public void actionPerformed(ActionEvent e)
+  {
+    String command = e.getActionCommand();
+    
+  }
 }
